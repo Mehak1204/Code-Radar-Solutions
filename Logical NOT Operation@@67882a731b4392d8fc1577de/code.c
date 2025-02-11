dd4aad1677eb84c9ae0ea47b5ae0a4046a077722
Mehak1204 <mphutela27@gmail.com>
2025-02-11 17:58:49 +0530

    Solution to Logical NOT Operation

---
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -0,0 +1,18 @@
+// Your code here...
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    
+    // Taking two space-separated integers as input
+    scanf("%d %d", &a, &b);
+    
+    // Checking if the numbers are equal
+    if (a !< 0 ) {
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
+    
+    return 0;
+}
